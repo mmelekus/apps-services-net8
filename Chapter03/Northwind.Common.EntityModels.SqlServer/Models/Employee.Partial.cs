9b7407a1784f38f390ae8d7cd992be27d37b82c7
MartinMZen <mmelekus@users.noreply.github.com>
2024-06-04 20:24:47 -0500

    Chapter 3: Building a reusable entity data model

---
+++ Chapter03/Northwind.Common.EntityModels.SqlServer/Models/Employee.Partial.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema; // [NotMapped]
+
+namespace Northwind.Common.EntityModels.SqlServer.Models;
+
+public partial class Employee : IHasLastRefreshed
+{
+    [NotMapped]
+    public DateTimeOffset LastRefreshed { get; set; }
+}
