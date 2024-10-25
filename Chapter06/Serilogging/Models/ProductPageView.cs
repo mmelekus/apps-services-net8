c6433ee9c5371e29f82fbb08020f07bde515e5fa
MartinMZen <mmelekus@users.noreply.github.com>
2024-10-25 13:59:28 -0500

    Chapter 6: Logging with Serilog

---
+++ Chapter06/Serilogging/Models/ProductPageView.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Serilogging.Models;
+
+public class ProductPageView
+{
+    public int ProductId { get; set; }
+    public string? PageTitle { get; set; }
+    public string? SiteSection { get; set; }
+}
