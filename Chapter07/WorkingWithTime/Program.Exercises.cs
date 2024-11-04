b2ee394775984f6d93b77b0a95397e9b2829b324
MartinMZen <mmelekus@hotmail.com>
2024-11-04 12:09:50 -0600

    Chapter 7: Formatting date and time values with culture

--- Chapter07/WorkingWithTime/Program.Exercises.cs
+++ Chapter07/WorkingWithTime/Program.Exercises.cs
@@ -2,8 +2,9 @@ partial class Program
 {
     private static void SpecifyingDateTimeValues(string culture = "en-US", bool overrideComputerCulture = true)
     {
-        ConfigureConsole(culture: culture, overrideComputerCulture: overrideComputerCulture); // Defaults to en-US culture.
         SectionTitle($"Specifying date and time values: {culture}");
+        ConfigureConsole(culture: culture, overrideComputerCulture: overrideComputerCulture); // Defaults to en-US culture.
+
         WriteLine($"DateTime.MinValue:  {DateTime.MinValue}");
         WriteLine($"DateTime.MaxValue:  {DateTime.MaxValue}");
         WriteLine($"DateTime.UnixEpoch: {DateTime.UnixEpoch}");
@@ -11,5 +12,23 @@ partial class Program
         WriteLine($"DateTime.Today:     {DateTime.Today}");
         WriteLine($"DateTime.Today:     {DateTime.Today:d}");
         WriteLine($"DateTime.Today:     {DateTime.Today:D}");
+        WriteLine();
+    }
+
+    private static void FormattingDateTimeValues(string culture = "en-US", bool overrideComputerCulture = true)
+    {
+        SectionTitle($"Formatting date and time values: {culture}");
+        ConfigureConsole(culture: culture, overrideComputerCulture: overrideComputerCulture); // Defaults to en-US culture
+
+        DateTime xmas = new(year: 2024, month: 12, day: 25);
+        WriteLine($"Christmas (default format): {xmas}");
+        WriteLine($"Christmas (custom short format): {xmas:ddd d/M/yy}");
+        WriteLine($"Christmas (custom long format): {xmas:dddd, dd MMMM yyyy}");
+        WriteLine($"Christmas (standard long format): {xmas:D}");
+        WriteLine($"Christmas (Sortable): {xmas:u}");
+        WriteLine($"Christmas is in month {xmas.Month} of the year.");
+        WriteLine($"Christmas is day {xmas.DayOfYear} of {xmas.Year}.");
+        WriteLine($"Christmas {xmas.Year} is on a {xmas.DayOfWeek}.");
+        WriteLine();
     }
 }
--- Chapter07/WorkingWithTime/Program.cs
+++ Chapter07/WorkingWithTime/Program.cs
@@ -1,3 +1,7 @@
-﻿SpecifyingDateTimeValues();
-SpecifyingDateTimeValues(culture: "en-GB");
-SpecifyingDateTimeValues(culture: "fr-CA");
+﻿// SpecifyingDateTimeValues();
+// SpecifyingDateTimeValues(culture: "en-GB");
+// SpecifyingDateTimeValues(culture: "fr-CA");
+
+FormattingDateTimeValues();
+FormattingDateTimeValues(culture: "en-GB");
+FormattingDateTimeValues(culture: "fr-CA");
