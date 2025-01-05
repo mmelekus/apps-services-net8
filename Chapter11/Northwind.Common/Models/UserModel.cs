bcf9181682031d438fb2a3e89b9df02f631c2c72
MartinMZen <mmelekus@hotmail.com>
2025-01-05 14:51:54 -0600

    Chapter 11: SignalR

---
+++ Chapter11/Northwind.Common/Models/UserModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Northwind.Common.Models;
+
+public class UserModel
+{
+    public string Name { get; set; } = null!;
+    public string ConnectionId { get; set; } = null!;
+    public string? Groups { get; set; }
+}
